5d4f75f20bfe68044de8edf899eaf60ff9214424
tunalad <28594843+tunalad@users.noreply.github.com>
2023-10-29 20:37:59 +0100

    possibly fixed the databases
    we made two databases originally, that might be the problem why I
    couldn't download from "any" arch

--- update-repo.sh
+++ update-repo.sh
@@ -3,17 +3,9 @@
 REPO_NAME="tunalad"
 ARCH=("any" "x86_64")
 
-update_arch (){
-    repo-add $REPO_NAME.db.tar.gz *.pkg.tar.zst
-    rm $REPO_NAME.db $REPO_NAME.files
-    mv $REPO_NAME.db.tar.gz $REPO_NAME.db
-    mv $REPO_NAME.files.tar.gz $REPO_NAME.files
-}
-
 for arch_dir in "${ARCH[@]}"; do
-    if cd $arch_dir; then
+    if ls "$arch_dir"; then
         echo -e "\033[1;32mUPDATING $arch_dir"
-        update_arch
-        cd ..
+        repo-add "$arch_dir/$REPO_NAME.db.tar.gz" "$arch_dir"/*.pkg.tar.zst
     fi
 done
