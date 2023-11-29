d0f49752b7d1984a8a7168e15dfc8fe4c45b13cb
tunalad <28594843+tunalad@users.noreply.github.com>
2023-11-29 19:21:23 +0100

    updated index.html, added eww packages

--- update-repo.sh
+++ update-repo.sh
@@ -7,5 +7,11 @@ for arch_dir in "${ARCH[@]}"; do
     if ls "$arch_dir"; then
         echo -e "\033[1;32mUPDATING $arch_dir"
         repo-add "$arch_dir/$REPO_NAME.db.tar.gz" "$arch_dir"/*.pkg.tar.zst
+        # removing symlinks
+        rm "$arch_dir/$REPO_NAME.db" "$arch_dir/$REPO_NAME.file"
+
+        # renaming archive files to symlink names
+        mv "$arch_dir/$REPO_NAME.db.tar.gz" "$arch_dir/$REPO_NAME.db"
+        mv "$arch_dir/$REPO_NAME.files.tar.gz" "$arch_dir/$REPO_NAME.files"
     fi
 done
